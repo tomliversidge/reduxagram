9e8ca99509763064251d804ef4947ccfb9063e14
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-05-21 20:55:02 +0100

    increment likes on like button click

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,11 @@
 function comments(state = [], action){
-    console.log(state, action);
-    return state;
+    switch (action.type){
+        case "ADD_COMMENT":
+            console.log("adding comment");
+        case "REMOVE_COMMENT":
+            console.log("removing comment");
+        default:
+            return state;
+    }
 }
 export default comments;
